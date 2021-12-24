6cfc902e3aaf9696f9492d20155915287af02125
unknown <pallabjyotisingha@gmail.com>
2021-12-24 20:59:59 +0530

    port 8080

--- app.py
+++ app.py
@@ -17,8 +17,7 @@ def start_flask_app():
     install_blueprints(app)
 
     # Start Flask Server
-    app.run(debug=True, port=8080, threaded=True,
-            use_reloader=True, use_debugger=True)
+    app.run()
 
 
 if __name__ == "__main__":
